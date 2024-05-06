965d16b135262ad1e1e970279fe96ccb9b626b4a
hoanganh191 <kbhoanganh05@gmail.com>
2024-05-06 17:58:40 +0700

    Bai 20A

---
+++ bai20A.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int n;
+    int tong_A = 0;
+    printf("Nhap gia tri cua n: ");
+    scanf("%d",&n);
+    while (n < 1) //vì đề bài xuất phát từ 1*2 + 2*3 nên n phải xuất phát từ 1
+    {
+        printf("Nhap lai so n > 1 : ");
+        scanf("%d",&n);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        tong_A = tong_A + i*(i+1);
+    }
+    printf("Gia tri cua tong A la : %d",tong_A);
+    
+}
