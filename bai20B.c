848581b9635f1651fb837ff388fe49873d1f2d28
hoanganh191 <kbhoanganh05@gmail.com>
2024-05-06 23:31:28 +0700

    Update

---
+++ bai20B.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int n;
+    int x;
+    float tong_B = 10; //Nhớ đề float vì cộng phân số 
+    printf("Nhap gia tri cua n va x: ");
+    scanf("%d %d",&n,&x);
+    while (x == 0 || n < 2) //Điều kiện x do đề bài còn n < 2 do phân số chyaj từ 2/x +....
+    {
+        printf("Nhap lai so thoa man n > 2 va x khac 0 : ");
+        scanf("%d",&n,&x);
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        tong_B = tong_B + (float)i/x;
+    }
+    printf("Gia tri cua tong B la : %f",tong_B);
+    
+}
