370a80f995341bdc9a403da4d0fc559b001a55d0
hoanganh191 <168956393+hoanganh191@users.noreply.github.com>
2024-05-06 18:05:42 +0700

    Update bai20A.c

--- bai20A.c
+++ bai20A.c
@@ -4,9 +4,9 @@ int main(){
     int tong_A = 0;
     printf("Nhap gia tri cua n: ");
     scanf("%d",&n);
-    while (n < 1) //vì đề bài xuất phát từ 1*2 + 2*3 nên n phải xuất phát từ 1
+    while (n < 0) //vì đề bài xuất phát từ n từ 0 mới chạy được
     {
-        printf("Nhap lai so n > 1 : ");
+        printf("Nhap lai so n > 0 : ");
         scanf("%d",&n);
     }
     for (int i = 1; i <= n; i++)
