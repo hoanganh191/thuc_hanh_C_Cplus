8e6e3ca4d977bddb04840bcdb39108666147a4c1
hoanganh191 <kbhoanganh05@gmail.com>
2024-05-07 23:30:39 +0700

    cap nhat bai 8

--- bai8.c
+++ bai8.c
@@ -4,10 +4,8 @@ int main(){
     printf("Nhap so co 3 chu so: ");
     scanf("%d",&a);
     don_vi = a%10;
-    a = a/10;
-    chuc = a%10;
-    a = a/10;
-    tram = a%10;
+    chuc = (a/10)%10; //Chia /10 nghĩa là lấy phần nguyên thành số có 2 chữ số rồi lấy phần dư để lấy giá trị hàng chục 
+    tram = (a/100)%10; 
     printf("Chu so hang tram la: %d\n",tram);
     printf("Chu so hang chuc la: %d\n",chuc);
     printf("Chu so hang tram la: %d",don_vi);
