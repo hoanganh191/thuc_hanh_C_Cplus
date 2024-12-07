ca056e1143bad026a00be9400131b7e05d19950f
Hoang_Anh <168956393+hoanganh191@users.noreply.github.com>
2024-12-07 21:16:43 +0700

    Cap nhat bai khanh hoi

--- Khanh.cpp
+++ Khanh.cpp
@@ -20,7 +20,7 @@ class Nguoi{
 		}
 		void xuat()
 		{
-			cout << "Hoten" << hoten << "\tGioitinh" << gioitinh << "\tNamsinh" << namsinh; 
+			cout << "Hoten : " << hoten << "\tGioitinh : " << gioitinh << "\tNamsinh : " << namsinh; 
 		}
 		int tinhTuoi()
 		{
@@ -48,7 +48,7 @@ class NhanSu : public Nguoi
 		void xuat()
 		{
 			Nguoi::xuat();
-			cout << "Mapb" << mapb << "\tChucvu" << chucvu << "\tNamct" << namcongtac; 
+			cout << "\tMapb : " << mapb << "\tChucvu : " << chucvu << "\tNamct : " << namcongtac << endl; 
 		}
 		string getMaphongban()
 		{
@@ -56,32 +56,40 @@ class NhanSu : public Nguoi
 		}
 };
 
-main()
+int main()
 {
 	NhanSu ns[10];
 	int i, n;
 	cout << "Nhap so luong nhan vien: "; cin >> n;
 	for (i = 0; i < n; ++i) 
 	{
-        cout << "Nhap thong tin nhan vien thu " << i << ":" << endl;
+        cout << "Nhap thong tin nhan vien thu " << i + 1 << ":" << endl;
         ns[i].nhap();
 	}
 	
+    cout << "----------------------------------";
 	cout << "\nDanh sach nhan vien phong hanh chinh: \n";
 	for(i =0; i<n ; i++)
 	{
-		if(ns[i].getMaphongban() == "HANH CHINH"){
+		if(ns[i].getMaphongban().compare("HANH CHINH") == 0){ //Nen su dung compare va viet hoa y het noi dung ben trong
 			ns[i].xuat();
 		}
 	}
 	
-	cout << "\nNhan vien co tuoi cao nhat: ";
-	int max = ns[0].tinhTuoi();
-	for(i =0; i<n ; i++)
-	{
-		if(ns[i].tinhTuoi() > max){
-			ns[i].tinhTuoi() == max;
-				ns[i].xuat();
-		}
-	}
+    
+    cout << "--------------------------------";
+    //Thuat toan sai can sua lai
+	cout << "\nNhan vien co tuoi cao nhat: " << endl;
+    int viTriTuoiMax = 0; // Bien nay se di tim vi tri nguoi co tuoi cao nhat trong mang, ta gia su nguoi dau tien cao tuoi nhat
+    for(int i = 1; i < n; i++){ //Vong lap de so sanh nhung nguoi con lai voi gia thuyet
+        if(ns[viTriTuoiMax].tinhTuoi() < ns[i].tinhTuoi()){ //Neu co nguoi nao do co nhieu tuoi hon, cap nhat vi tri
+            viTriTuoiMax = i; //Cap nhat vi tri nguoi co nhieu tuoi nhat la nguoi do
+        }
+    }
+
+    //Vong lap ket thuc, bay gio bien viTriTuoiMax luu vi tri cua nguoi nhieu tuoi nhat
+    ns[viTriTuoiMax].xuat();
+
+
+	
 }
