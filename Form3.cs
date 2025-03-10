4e744a80d6d931e16e1454366981d15cf875d4d3
Hoang_Anh <168956393+hoanganh191@users.noreply.github.com>
2025-03-10 20:10:53 +0700

    capnhat

--- Form3.cs
+++ Form3.cs
@@ -30,6 +30,7 @@ namespace Tuan6
             con.Close();    
             dgvsach.DataSource = dt;
             dgvsach.Refresh();
+            dgvsach.Columns["Mota"].Visible= false;
         }
 
         private void Q(object sender, DataGridViewCellEventArgs e)
@@ -46,6 +47,8 @@ namespace Tuan6
             string maloai=txtmaloai.Text;
             string tenloai=txttenloai.Text;
             string mota=txtmota.Text;//để nhập giá trị
+            string soluongchu=txtsoluong.Text;
+            int soluong=int.Parse(soluongchu);
             if(maloai=="")
             {
                 MessageBox.Show("Bạn chưa nhập mã loại");
@@ -55,12 +58,18 @@ namespace Tuan6
             {
                 MessageBox.Show("Mã loại trùng"); return;
             }    
+            if(soluong<=90)
+            {
+                MessageBox.Show("số lương ko hợp lệ");
+                return;
+            }    
             if(con.State == ConnectionState.Closed)  {con.Open();}
-            string sql= "insert into sach values(@maloai,@tenloai,@mota)";
+            string sql= "insert into sach values(@maloai,@tenloai,@mota,@soluong)";
             SqlCommand cmd= new SqlCommand(sql, con);
             cmd.Parameters.Add("@maloai", SqlDbType.NVarChar, 50).Value = maloai;
             cmd.Parameters.Add("@tenloai",SqlDbType.NVarChar,50).Value=tenloai;
             cmd.Parameters.Add("@mota",SqlDbType.NVarChar,50).Value = mota;//lưu giá trị vào  sql
+            cmd.Parameters.Add("@soluong",SqlDbType.Int).Value=soluong;
             cmd.ExecuteNonQuery();
             cmd.Dispose ();
             con.Close ();
@@ -90,6 +99,7 @@ namespace Tuan6
             txtmaloai.Text= dgvsach.Rows[hangdangchon].Cells[0].Value.ToString();
             txttenloai.Text = dgvsach.Rows[hangdangchon].Cells[1].Value.ToString();
             txtmota.Text = dgvsach.Rows[hangdangchon].Cells[2].Value.ToString();
+            txtsoluong.Text = dgvsach.Rows[hangdangchon].Cells[3].Value.ToString();
         }
 
         private void btnxoa_Click(object sender, EventArgs e)
@@ -120,12 +130,14 @@ namespace Tuan6
             string maloai= txtmaloai.Text;
             string tenloai= txttenloai.Text;
             string mota= txtmota.Text;
+            string soluong=txtsoluong.Text;
             if (con.State == ConnectionState.Closed) con.Open();
-            string sql = "update sach set Tenloai=@tenloai,Mota=@tenloai where Maloai=@maloai";
+            string sql = "update sach set Tenloai=@tenloai,Mota=@tenloai,Soluong=@soluong where Maloai=@maloai";
             SqlCommand cmd= new SqlCommand(sql, con);
             cmd.Parameters.Add("@maloai",SqlDbType.NVarChar,50).Value = maloai;
             cmd.Parameters.Add("@tenloai",SqlDbType.NVarChar,50).Value= tenloai;
             cmd.Parameters.Add("@mota",SqlDbType.NVarChar,50).Value= mota;
+            cmd.Parameters.Add("@soluong",SqlDbType.Int).Value= soluong;
             cmd.ExecuteNonQuery();
             cmd.Dispose();
             con.Close();
@@ -138,13 +150,16 @@ namespace Tuan6
             string maloai = txtmaloai.Text;
             string tenloai = txttenloai.Text;
             string mota= txtmota.Text;
+            string soluongchu = txtsoluong.Text;
+            //int soluong=int.Parse(soluongchu);
             if(con.State == ConnectionState.Closed)
                 con.Open();
-            string sql = "select * from sach where Maloai like @maloai and Tenloai like @tenloai and Mota like @mota";
+            string sql = "select * from sach where Maloai like @maloai and Tenloai like @tenloai and Mota like @mota ";
             SqlCommand cmd = new SqlCommand(sql,con);
             cmd.Parameters.Add("@maloai",SqlDbType.NVarChar,50).Value="%"+maloai+"%";
             cmd.Parameters.Add("@tenloai", SqlDbType.NVarChar, 50).Value = "%"+tenloai+"%";
             cmd.Parameters.Add("@mota",SqlDbType.NVarChar,50).Value="%"+mota+"%";
+            //cmd.Parameters.Add("@soluong", SqlDbType.Int).Value = soluong;
             SqlDataAdapter adapter = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
